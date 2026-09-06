c47bc9dc4541042d797026a541737d1edb5b5f49
Jatin Rathore <jatinrathore1234567@gmail.com>
2026-09-07 00:42:21 +0530

    master | commit 1 | chore: Remove practice components, keep progress bars demo     - Deleted accordion, todo, debounce, modal, star rating and infinite scroll demos.     - Removed bars, default intro and reducer practice folders from src.     - Pointed App.jsx to concurrent progress bars component only.

--- src/bars/header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = ({ heading }) => {
-  return (
-    <span style={{ fontSize: "20px", fontWeight: 600, color: "#637381" }}>
-      {heading}{" "}
-    </span>
-  );
-};
-
-export default Header;
